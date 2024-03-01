ada92cce3d7458e49d50372aad0a491f168172ee
babak <111069294+babakyousefian@users.noreply.github.com>
2024-03-01 07:08:10 -0800

    add and update some news via phpmyadmin
    add and update some news via phpmyadmin

--- add.blade.php
+++ add.blade.php
@@ -11,15 +11,30 @@
     @if(Session::has('success_message'))
         <div class="form_success_message">{{Session::get("success_message")}}</div>
     @endif
-
-    <form action="{{URL::asset("admin/category/add_check")}}" method="post" enctype="multipart/form-data">
+    <form action="{{URL::asset("admin/news/add_check")}}" method="post" enctype="multipart/form-data">
         {{csrf_field()}}
-        <div class="form_input_title">عنوان دسته:</div>
-        <input type="text" name="name" class="form_input">
-        @if($errors->has('name'))
-            <div class="error_msg">{{$errors->first('name')}}</div>
+        <div class="form_input_title">عنوان خبر:</div>
+        <input type="text" name="title" class="form_input">
+        @if($errors->has('title'))
+            <div class="error_msg">{{$errors->first('title')}}</div>
         @endif
-        <input type="submit" value="افزودن دسته جدید" class="form_btn">
+        <div class="form_input_title">تصویر خبر:</div>
+        <input type="file" name="image" class="form_file">
+        @if($errors->has('image'))
+            <div class="error_msg">  {{$errors->first('image')}}</div>
+        @endif
+        <div class="form_input_title">دسته بندی خبر:</div>
+        <select name="category" class="form_input">
+{{--            <option value="sports">ورزشی</option>--}}
+           @foreach($categories as $category)
+               <option value="{{$category->id}}">{{$category->name}}</option>
+           @endforeach
+        </select>
+        <div class="form_input_title">توضیحات خبر:</div>
+        <textarea  name="description" class="form_text"></textarea>
+        @if($errors->has('description'))
+            <div class="error_msg">  {{$errors->first('description')}}</div>
+        @endif
+        <input type="submit" value="افزودن خبر جدید" class="form_btn">
     </form>
-
 @endsection
--- show.blade.php
+++ show.blade.php
@@ -8,23 +8,24 @@
     @if(Session::has('success_message'))
         <div class="form_success_message">{{Session::get("success_message")}}</div>
     @endif
-    <table class="table table-bordered table-hover">
-        <tr class="info">
-            <th>#</th>
-            <th>عنوان</th>
-            <th>ویرایش</th>
-            <th>حذف</th>
-        </tr>
-        @php $i=1;@endphp
-        @foreach($categories as $category)
-
-            <tr>
-                <td>{{$i++}}</td>
-                <td>{{$category["name"]}}</td>
-                <td><a href="{{URL::asset("admin/category/update/".$category["id"])}}">ویرایش</a></td>
-                <td><a href="{{URL::asset("admin/category/delete/".$category["id"])}}">حذف</a></td>
-            </tr>
+<table class="table table-bordered table-hover">
+    <tr class="info">
+        <th>#</th>
+        <th>عنوان</th>
+        <th>تاریخ</th>
+        <th>ویرایش</th>
+        <th>حذف</th>
+    </tr>
+    @php $i=1;@endphp
+    @foreach($news as $n)
+    <tr>
+        <td>{{$i++}}</td>
+        <td>{{$n["title"]}}</td>
+        <td>{{$n["date"]}}</td>
+        <td><a href="{{URL::asset("admin/news/update/".$n["id"])}}">ویرایش</a></td>
+        <td><a href="{{URL::asset("admin/news/delete/".$n["id"])}}">حذف</a></td>
+    </tr>
         @endforeach
-    </table>
-    {{$categories->links()}}
+</table>
+    {{$news->links()}}
 @endsection
