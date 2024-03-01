40da534d903d10337ede9cfb27e2c8b0240f3f04
babak <111069294+babakyousefian@users.noreply.github.com>
2024-03-01 06:28:22 -0800

    Add files via upload
    provider

---
+++ BroadcastServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * Author = babak yousefian
+ */
+
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\ServiceProvider;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Broadcast::routes();
+
+        require base_path('routes/channels.php');
+    }
+}
