e6a199abf1fb7deb3ab079ad5b37c59f5c502cbc
babak <111069294+babakyousefian@users.noreply.github.com>
2024-03-01 06:19:06 -0800

    Add files via upload
    Middleware

---
+++ Authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * Author = babak yousefian
+ */
+
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
+}
