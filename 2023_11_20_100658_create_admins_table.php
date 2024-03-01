7ea2bc061e0a0db95b525b70c527985d9cb808bc
babak <111069294+babakyousefian@users.noreply.github.com>
2024-03-01 06:44:59 -0800

    add some migration for phpmyadmin
    add some migration for phpmyadmin

---
+++ 2023_11_20_100658_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/**
+ * Author = babak yousefian
+ */
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('profile');
+            $table->string('email');
+            $table->string('password');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
